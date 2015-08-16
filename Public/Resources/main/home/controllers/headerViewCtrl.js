2e4f133d5354c0f43ae2eec7e2e24d4837642233
sdorle <shreyas.dorle7@gmail.com>
2015-08-16 14:18:03 -0700

    Test Commit

--- Public/Resources/main/home/controllers/headerViewCtrl.js
+++ Public/Resources/main/home/controllers/headerViewCtrl.js
@@ -29,4 +29,5 @@ app.controller('headerViewCtrl', ['$scope', '$rootScope', '$location', function(
 	$scope.setRoute = function (index) {
 		$location.path(($scope.navigationItems[index].name).toLowerCase());
 	};
+	
 }]);
